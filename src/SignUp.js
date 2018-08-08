f1fe2c0a8edd2d60430771266dbe9ae554793608
Ra Wan <rawan@Rawan-2.local>
2018-08-08 07:13:48 +0300

    initial

---
+++ src/SignUp.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  render() {
+    return (
+     <div> 
+       <h1> Sign Up</h1>
+     </div>
+
+
+    );
+  }
+}
+ 
+export default SignUp;
